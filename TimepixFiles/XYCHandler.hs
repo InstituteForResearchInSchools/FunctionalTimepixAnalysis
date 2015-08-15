481ef3a1fd7a59f81b564bd564936c591268eab7
Matt Harrison <matt.anthony.harrison@gmail.com>
2015-08-15 12:48:47 +0100

    First code upload
    This code contains basic I/O in of XYC files and a simple clustering
    algorithm queueCluster

---
+++ TimepixFiles/XYCHandler.hs
@@ -0,0 +1,13 @@
+module XYCHandler
+( formatLines
+) where
+
+formatLines :: [String] -> [(Int,Int,Float)]
+formatLines []     = []
+formatLines (l:ls) = let 
+                         (x:y:c:[]) = words l
+                         x' = read x
+                         y' = read y
+                         c' = read c
+                     in   
+                         (x',y',c') : (formatLines ls)
